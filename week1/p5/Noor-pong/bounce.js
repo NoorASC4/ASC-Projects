c6779e3329070124a0652c8f83c8a563a7049834
NoorASC4 <rajn@bxscience.edu>
2017-07-19 02:14:01 -0400

    late night work

--- week1/p5/Noor-pong/bounce.js
+++ week1/p5/Noor-pong/bounce.js
@@ -12,34 +12,73 @@ var xpos = 50;
 var ypos = 50;
 
 //think of this fucntion as the main function which is calling everything else
+var rectx = 100;
+var recty = 550;
+var rectxspd = 5;
+
 function draw(){ 
     background('pink');
-    fill("yellow")
-    ellipse(xpos, ypos, 100, 100);
+    fill("yellow");
+    ellipse(xpos, ypos, 75, 75);
     ballMove();
     wordy();
-       
+    strokeWeight(2)
+    fill("white");
+
+    if(keyIsDown(RIGHT_ARROW)) {
+        rectx += rectxspd;
+    };
+    if(keyIsDown(LEFT_ARROW)) {
+        rectx -= rectxspd;
+    };
+
+    ballTouchesPaddle();
+
 }
 
 //this function basically just causes the ball
 //to actually move and 'bounce off the walls
 function ballMove(){
-    ellipse(xpos, ypos, 100);
+    ellipse(xpos, ypos, 75);
     xpos = xpos + xspeed;
     ypos += yspeed;
     if (xpos >= (width-50) || xpos < 50) {
         xspeed = (-xspeed);
     }
-    if (ypos >= (height-50) || ypos <=50) {
+    if ( ypos <=50) {
         yspeed = (-yspeed);
     }    
 }
+
+function ballTouchesPaddle(){
+    rect(rectx, recty - 15, 100, 8);
+
+    if (ypos == (recty- 50) ) {
+        yspeed = (-yspeed)
+    }
+}
+
+/*
+function key({
+    if(keyIsPressed()=== true){
+        rectx += rectxspd;
+    }
+    /*
+    if (keyIsPressed() === ) {
+        rextx -= rectxspd;
+    }
+
+}
+*/
+
+
+
 function wordy(){
     fill(255);
     stroke("black");
     strokeWeight(3);
     textAlign(CENTER, CENTER);
-    textSize(30);
+    textSize(25);
     text("O  O",xpos, ypos);
     line(xpos - 10, ypos + 20, xpos+10, ypos +20);
 
@@ -54,3 +93,4 @@ function changeColor() {
     noStroke();
     fill(num, num2, num3);
 }
+
--- week1/p5/Noor-typetest/sketch.js
+++ week1/p5/Noor-typetest/sketch.js
@@ -1,11 +1,12 @@
 //function setup creates a canvas with a grey background
-function setup(){
-    
+
+x = 0
+
+function setup(){ 
     createCanvas(windowWidth/2,windowHeight/2);
     background(225);
-    
 }
-x = 0
+
 function draw(){
     
     textSize(32);
@@ -13,16 +14,28 @@ function draw(){
     text("Type the Song Lyrics!",50,50);
 
     textSize(20);
+    background(225);
     text("Type this 1",50, 100);
     text("Type this 2",50, 150);
     text("Type this 3",50, 200);
     text("Type this 4",50, 250);
-    text("Points Earned : " + x,50, 350);
+    text("Points Earned : " + x ,50, 350);
 }
 
-
+var str1 = "Type"
+function createArray(str){
+    myArray = [];
+    for (i=0; i < str1.length; i++) {
+        myArray.push(str1.charAt(i));
+    }
+    return myArray;
+}
+console.log(createArray(str1));
 function keyTyped (){
-    if (key === "A"){
-        x += 10;
+    myArray = createArray(str1);
+    for (var i = 0; i < 4; i++){
+        if (key === myArray[i]){
+            x+=10;
+        }
     }
 }
